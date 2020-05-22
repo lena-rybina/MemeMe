616704946ce0ae5579c79fa7404ce64eabf63af7
lena-rybina <ielena.rybina@gmail.com>
2020-05-22 18:50:55 -0400

    Initial Commit

---
+++ MemeMe v1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe v1.0
+//
+//  Created by Ielena Rybina on 5/22/20.
+//  Copyright © 2020 Elena Rybina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
